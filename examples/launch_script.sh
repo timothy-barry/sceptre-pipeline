e6e63b4a96a936525aa614e44410a9c8408f54a8
Tim Barry <tbarry2@andrew.cmu.edu>
2024-01-30 17:24:58 -0500

    simplify examples

--- examples/launch_script.sh
+++ examples/launch_script.sh
@@ -6,24 +6,23 @@ export NXF_OPTS="-Xms500M -Xmx4G"
 ##########################
 # REQUIRED INPUT ARGUMENTS
 ##########################
-source ~/.research_config
-gasp_dir=$LOCAL_GASPERINI_2019_DATA_DIR"small_scale/"
+ex_dir="/Users/timbarry/sceptre_example/"
 # sceptre object
-sceptre_object_fp=$gasp_dir"sceptre_object.rds"
+sceptre_object_fp=$ex_dir"sceptre_object.rds"
 # response ODM
-response_odm_fp=$gasp_dir"gene.odm"
+response_odm_fp=$ex_dir"gene.odm"
 # grna ODM
-grna_odm_fp=$gasp_dir"grna.odm"
+grna_odm_fp=$ex_dir"grna.odm"
 
 ###################
 # OUTPUT DIRECTORY:
 ##################
-output_directory="/Users/timbarry/gasperini_pipeline_outputs"
+output_directory="/Users/timbarry/sceptre_example/pipeline_outputs"
 
 #################
 # Invoke pipeline
 #################
-nextflow run ../../main.nf \
+nextflow run ../../main.nf -resume \
  --sceptre_object_fp $sceptre_object_fp \
  --response_odm_fp $response_odm_fp \
  --grna_odm_fp $grna_odm_fp \
@@ -31,5 +30,5 @@ nextflow run ../../main.nf \
  --grna_assignment_method "mixture" \
  --pair_pod_size "30" \
  --grna_pod_size "10" \
- --pipeline_stop "run_discovery_analysis" \
- -resume
+ --pipeline_stop "run_discovery_analysis"
+ 
