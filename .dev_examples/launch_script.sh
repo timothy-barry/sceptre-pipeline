08e15fc22ffeb4635db3b4926f840417a3c52933
Tim Barry <tbarry2@andrew.cmu.edu>
2024-01-30 17:50:19 -0500

    upate example launch script

--- .dev_examples/launch_script.sh
+++ .dev_examples/launch_script.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
+
 # Limit NF driver to 4 GB memory
 export NXF_OPTS="-Xms500M -Xmx4G"
 
 ##########################
 # REQUIRED INPUT ARGUMENTS
 ##########################
-example_dir="/tmp/sceptre_example/"
+ex_dir="/Users/timbarry/sceptre_example/"
 # sceptre object
-sceptre_object_fp=$example_dir"sceptre_object.rds"
+sceptre_object_fp=$ex_dir"sceptre_object.rds"
 # response ODM
-response_odm_fp=$example_dir"gene.odm"
+response_odm_fp=$ex_dir"gene.odm"
 # grna ODM
-grna_odm_fp=$example_dir"grna.odm"
+grna_odm_fp=$ex_dir"grna.odm"
 
 ###################
 # OUTPUT DIRECTORY:
 ##################
-output_directory=$example_dir"/pipeline_outputs"
+output_directory="/Users/timbarry/sceptre_example/pipeline_outputs"
 
 #################
 # Invoke pipeline
 #################
-nextflow run timothy-barry/sceptre-pipeline -resume \
+nextflow run ../../main.nf -resume \
  --sceptre_object_fp $sceptre_object_fp \
  --response_odm_fp $response_odm_fp \
  --grna_odm_fp $grna_odm_fp \
@@ -29,5 +30,5 @@ nextflow run timothy-barry/sceptre-pipeline -resume \
  --grna_assignment_method "mixture" \
  --pair_pod_size "30" \
  --grna_pod_size "10" \
- --trial "true"
+ --pipeline_stop "run_discovery_analysis"
  
