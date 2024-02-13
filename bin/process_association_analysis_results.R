582f6047d1ee02ff1ee6c03a244f6cb0dc18fb66
Tim Barry <tbarry2@andrew.cmu.edu>
2024-02-13 18:56:43 -0500

    begin pruning of process_assoc_analysis_results

--- bin/process_association_analysis_results.R
+++ bin/process_association_analysis_results.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-# obtain the command line arguments
+# 1. obtain the command line arguments
 args <- commandArgs(trailingOnly = TRUE)
 sceptre_object_fp <- args[1]
 response_odm_fp <- args[2]
@@ -9,15 +9,14 @@ analysis_type <- args[4]
 result_fps <- grep(x = args, pattern = "results*", value = TRUE)
 precomp_fps <- grep(x = args, pattern = "precomputations*", value = TRUE)
 
-# load the sceptre object
-# load the sceptre object
+# 2. load the sceptre object; update completed analysis flags
 sceptre_object <- sceptre::read_ondisc_backed_sceptre_object(sceptre_object_fp = sceptre_object_fp,
                                                              response_odm_file_fp = response_odm_fp,
                                                              grna_odm_file_fp = grna_odm_fp)
 sceptre_object@functs_called[[analysis_type]] <- TRUE
 sceptre_object@last_function_called <- analysis_type
 
-# process the results; add to sceptre_object
+# 3. process the results
 result_df <- lapply(result_fps, readRDS) |> data.table::rbindlist()
 data.table::setorderv(result_df, cols = c("p_value", "response_id"), na.last = TRUE)
 process_funct <- switch(analysis_type,
@@ -26,21 +25,36 @@ process_funct <- switch(analysis_type,
                         run_discovery_analysis = sceptre:::process_discovery_result)
 result_df <- process_funct(result_df, sceptre_object)
 result_df$pod <- NULL
-result_name <- switch(analysis_type,
-                      run_calibration_check = "calibration_result",
-                      run_power_check = "power_result",
-                      run_discovery_analysis = "discovery_result")
-slot(sceptre_object, result_name) <- result_df
 
-# process the response precomputations
-precomputation_list <- lapply(precomp_fps, readRDS) |> unlist(recursive = FALSE)
-sceptre_object@response_precomputations <- c(sceptre_object@response_precomputations,
-                                             precomputation_list)
+# 4. add to the sceptre_object
+if (analysis_type == "run_calibration_check") {
+  sceptre_object@calibration_result <- result_df
+} else if (analysis_type == "run_power_check") {
+  sceptre_object@power_result <- result_df
+} else { # discovery analysis
+  sceptre_object@discovery_result <- result_df
+}
 
-# create plot
+# 5. process the response precomputations (if using the complement set as the control group)
+if (sceptre_object@control_group_complement) {
+  precomputation_list <- lapply(precomp_fps, readRDS) |> unlist(recursive = FALSE)
+  sceptre_object@response_precomputations <- c(sceptre_object@response_precomputations,
+                                               precomputation_list)    
+}
+
+# 6. create the plot
 p <- sceptre::plot(sceptre_object)
 
-# save outputs
+# 7. delete unecessary info from sceptre_object
+if (analysis_type == "run_calibration_check") {
+  # pass
+} else if (analysis_type == "run_power_check") {
+  # pass
+} else { # discovery analysis
+  sceptre_object <- NULL
+}
+
+# save the outputs
 saveRDS(sceptre_object, "sceptre_object.rds")
 saveRDS(result_df, paste0("results_", analysis_type, ".rds"))
 ggplot2::ggsave(filename = paste0("plot_", analysis_type, ".png"), plot = p, device = "png", scale = 1.1, width = 5, height = 4, dpi = 330)
